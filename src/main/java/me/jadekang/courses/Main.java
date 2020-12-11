ad0b62f63d307687f7fa8d35c177c18709f6250f
jadekang <jkang.se@gmail.com>
2020-12-10 19:43:16 -0800

    added sign in hbs and post routing.

--- src/main/java/me/jadekang/courses/Main.java
+++ src/main/java/me/jadekang/courses/Main.java
@@ -2,13 +2,23 @@ package me.jadekang.courses;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import static spark.Spark.*;
+import java.util.Map;
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 
 public class Main {
     public static void main(String[] args) {
-
+        port(8000);
         get("/", (req, res) -> {
             return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req, res) -> {
+            return new ModelAndView(null, "sign-in.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
