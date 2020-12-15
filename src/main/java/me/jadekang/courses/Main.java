db23fb06ed90fe5b3da6564e07174e0ea1d271b3
jadekang <jkang.se@gmail.com>
2020-12-15 12:18:25 -0800

    Added DAO interface for my prototype.

--- src/main/java/me/jadekang/courses/Main.java
+++ src/main/java/me/jadekang/courses/Main.java
@@ -1,4 +1,6 @@
 package me.jadekang.courses;
+import me.jadekang.courses.model.CourseIdeaDAO;
+import me.jadekang.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -12,13 +14,23 @@ import static spark.Spark.post;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Make my model object
+        CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
         port(8000);
         get("/", (req, res) -> {
-            return new ModelAndView(null, "index.hbs");
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/sign-in", (req, res) -> {
-            return new ModelAndView(null, "sign-in.hbs");
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            res.cookie("username", username);
+            model.put("username", username);
+            return new ModelAndView(model, "sign-in.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
