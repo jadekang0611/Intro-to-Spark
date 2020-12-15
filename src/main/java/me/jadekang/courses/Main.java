6e040ab76a8f93603f7f58ca96167159bc5aa161
jadekang <jkang.se@gmail.com>
2020-12-15 14:40:56 -0800

    Added a main css for the overall page styling and built adding a new idea logic with a new route and view.

--- src/main/java/me/jadekang/courses/Main.java
+++ src/main/java/me/jadekang/courses/Main.java
@@ -1,4 +1,5 @@
 package me.jadekang.courses;
+import me.jadekang.courses.model.CourseIdea;
 import me.jadekang.courses.model.CourseIdeaDAO;
 import me.jadekang.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
@@ -7,18 +8,16 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.Map;
 import java.util.HashMap;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
-import static spark.Spark.post;
+import static spark.Spark.*;
 
 
 public class Main {
     public static void main(String[] args) {
-
+        staticFileLocation("/public.css");
         // Make my model object
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
 
-        port(8000);
+
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
             model.put("username", req.cookie("username"));
@@ -31,6 +30,22 @@ public class Main {
             res.cookie("username", username);
             model.put("username", username);
             return new ModelAndView(model, "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/ideas", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("ideas", dao.findAll());
+            return new ModelAndView(model, "ideas.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (req, res) -> {
+            String title = req.queryParams("title");
+
+            // TODO: jadekang - This user name is tied to the cookie implementation
+            CourseIdea courseIdea = new CourseIdea(title, req.cookie("username"));
+            dao.add(courseIdea);
+            res.redirect("/ideas");
+            return null;
         }, new HandlebarsTemplateEngine());
     }
 }
